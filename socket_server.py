c6b953489a33204fbe9c49d51f7573d0a86d0145
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-05-30 00:16:51 -0600

    Update socket_server.py

--- socket_server.py
+++ socket_server.py
@@ -4,18 +4,22 @@ import socket
 import celery
 
 from base64 import b64encode, b64decode
-from rsasigner import rsatn
+from rsa_celery_daemon import rsatn
 
-class Handler_TCPServer(socketserver.BaseRequestHandler):
+class handler_tcp(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print("{} sent:".format(self.client_address[0]))
         print(self.data)
         token_crypto = str(self.data)
-        results = str(rsatn("ADMIN", token_crypto))
-        self.request.sendall(results.encode())
+        try:
+            results = str(rsatn("ADMIN", token_crypto))
+        except Exception:
+            results = "Server Error"
+        finally:
+            self.request.sendall(results.encode())
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9948
-    tcp_server = socketserver.TCPServer((HOST, PORT), Handler_TCPServer)
+    tcp_server = socketserver.TCPServer((HOST, PORT), handler_tcp)
     tcp_server.serve_forever()
