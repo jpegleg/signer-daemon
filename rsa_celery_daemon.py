897871e63aaaf0f694dbfcc32ef090962cd7d5b3
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-05-29 23:17:47 -0600

    Update rsa_celery_daemon.py

--- rsa_celery_daemon.py
+++ rsa_celery_daemon.py
@@ -24,7 +24,7 @@ app.setup_security()
 
 @app.task
 def rsatn(self, token_crypto):
-    tag = bytes("RSASIGN",encoding="utf8")
+    tag = bytes("RSA_SIGNED->",encoding="utf8")
     with open('rsa.pem','r') as f:
         privkey = rsa.PrivateKey.load_pkcs1(f.read().encode())
     token_message = token_crypto
