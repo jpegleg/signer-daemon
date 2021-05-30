737839736d9cefe46122108ee082a799481d1b9e
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-05-30 03:45:46 -0600

    Create verify_server.py

---
+++ verify_server.py
@@ -0,0 +1,20 @@
+import socketserver
+import rsa
+import socket
+import celery
+
+from base64 import b64encode, b64decode
+from rsa_celery_daemon import rsavf
+
+class handle_tcp(socketserver.BaseRequestHandler):
+    def handle(self):
+        self.data = self.request.recv(1024).strip()
+        print("{} sent:".format(self.client_address[0]))
+        print(self.data)
+        results = str(rsavf(self.data))
+        self.request.sendall(results.encode())
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 9949
+    tcp_server = socketserver.TCPServer((HOST, PORT), handle_tcp)
+    tcp_server.serve_forever()
