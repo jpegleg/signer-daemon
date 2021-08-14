3131436c09a6952cdd670368c7bd1fd22743b6c8
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-08-14 15:15:53 -0600

    Update ecdsa_celery_daemon.py

--- ecdsa_celery_daemon.py
+++ ecdsa_celery_daemon.py
@@ -25,7 +25,7 @@ app.setup_security()
 
 @app.task
 def dsatn(message_in):
-    """ dsa signature create and insert into redis """
+    """ dss signature create and insert into redis """
     re_dis = redis.StrictRedis(host='localhost', port=6379, db=0)
     f_key = open('secp384r1.pem', 'r')
     key_pair = ECC.import_key(f_key.read())
@@ -36,7 +36,7 @@ def dsatn(message_in):
     print("Signature:", binascii.hexlify(signature))
     return(message_in, binascii.hexlify(signature))
 def dsavf(message_in):
-    """ dsa signature verify """
+    """ dss signature verify """
     re_dis = redis.StrictRedis(host='localhost', port=6379, db=0)
     f_key = open('secp384r1.pub', 'r')
     key_pair = ECC.import_key(f_key.read())
