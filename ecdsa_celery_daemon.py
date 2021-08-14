adb05fdea7db57dea72a92c1ab930c56c7ecc117
Keegan Bowen <1106191+jpegleg@users.noreply.github.com>
2021-08-14 12:49:31 -0600

    Update ecdsa_celery_daemon.py

--- ecdsa_celery_daemon.py
+++ ecdsa_celery_daemon.py
@@ -1,5 +1,5 @@
-import redis
 import binascii
+import redis
 
 from celery import Celery
 from Crypto.PublicKey import ECC
@@ -22,32 +22,34 @@ app.conf.update(
 )
 app.setup_security()
 
-@app.task
+@APP.task
 def dsatn(message_in):
-    r = redis.StrictRedis(host='localhost', port=6379, db=0)
-    f = open('secp384r1.pem','r')
-    keyPair = ECC.import_key(f.read())
+    """ dsa signature create and insert into redis """
+    re_dis = redis.StrictRedis(host='localhost', port=6379, db=0)
+    f_key = open('secp384r1.pem', 'r')
+    key_pair = ECC.import_key(f_key.read())
     hash = SHA384.new(message_in)
-    signer = DSS.new(keyPair, 'fips-186-3')
+    signer = DSS.new(key_pair, 'fips-186-3')
     signature = signer.sign(hash)
-    r.mset({binascii.hexlify(signature): message_in})
+    re_dis.mset({binascii.hexlify(signature): message_in})
     print("Signature:", binascii.hexlify(signature))
     return(message_in, binascii.hexlify(signature))
 def dsavf(message_in):
-    r = redis.StrictRedis(host='localhost', port=6379, db=0)
-    f = open('secp384r1.pub','r')
-    keyPair = ECC.import_key(f.read())
-    message = r.get(message_in)
-    hash = SHA384.new(message)
-    signer = DSS.new(keyPair, 'fips-186-3')
+    """ dsa signature verify """
+    re_dis = redis.StrictRedis(host='localhost', port=6379, db=0)
+    f_key = open('secp384r1.pub', 'r')
+    key_pair = ECC.import_key(f_key.read())
+    message = re_dis.get(message_in)
+    hashd = SHA384.new(message)
+    signer = DSS.new(key_pair, 'fips-186-3')
     signature = binascii.unhexlify(message_in)
     print("Verification of data:", message)
     try:
-      validate = signer.verify(hash, signature)
-      return('Valid entry found.')
+        validate = signer.verify(hashd, signature)
+        return('Valid entry found.')
     except Exception as error:
-      print('ERROR: ', error)
-      return('No match for signature.')
+        print('ERROR: ', error)
+        return('No match for signature.')
 
 if __name__ == '__main__':
-    app.start()
+    APP.start()
